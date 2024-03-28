ba691c14cd81b9c75b0c69a11fec0bf5eaa286b9
Anders Stubberud <andersvs02@gmail.com>
2024-03-28 23:25:36 +0100

    add to requirements

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -7,8 +7,8 @@ import { MainNav } from "@/components/main-nav"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+    <header className="ticky top-0 z-40 w-full flex justify-center">
+      <div className="container flex h-16 items-center bg-background border-b space-x-4 sm:justify-between sm:space-x-0">
         <MainNav/>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
